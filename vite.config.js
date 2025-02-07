d96bedc5ad3bd3d0caa0a537dca9f01f87124b07
ericpo1sh <ericpo1sh@gmail.com>
2025-02-06 20:43:58 -0600

    changing location of files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Morgan-Custom-Windows/", // 👈 Add your repository name here
+  base: "/",
 });
