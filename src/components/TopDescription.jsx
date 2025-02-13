83ac21b89b4020669bfecf48c96e927538fe6fa8
ericpo1sh <ericpo1sh@gmail.com>
2025-02-12 22:35:01 -0600

    saving contact tag

--- src/components/TopDescription.jsx
+++ src/components/TopDescription.jsx
@@ -3,7 +3,7 @@ import '../styles/TopDescription.css'
 function TopDescription() {
   return (
     <div className='DescriptionContainer'>
-      <p>Premium Residential & Commercial Window Company in Northeastern Oklahoma • <span><a href='#'>Schedule Estimate Today →</a></span></p>
+      <p>Premium Residential & Commercial Window Company in Northeastern Oklahoma • <span><a href='/contact/'>Schedule Estimate Today →</a></span></p>
     </div>
   )
 }
